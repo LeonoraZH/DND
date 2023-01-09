fde719fa1a30f532ec86bc0534fce38afec1cfce
Leonora Zharova <norka_200@mail.rugit config --global user.namegit config --global user.email norka_200@mail.ru>
2023-01-09 11:14:03 +0300

    dnd

---
+++ src/app.js
@@ -0,0 +1,53 @@
+let dradAndDrop = document.querySelector(".drag-and-drop");
+console.log(dradAndDrop);
+
+const filesList = [];
+const arrayOfObjects = [];
+
+let uploadBtn = document.getElementById("upload-btn");
+let p;
+
+dradAndDrop.addEventListener("dragenter", function (e) {
+  e.preventDefault();
+  dradAndDrop.classList.add("active");
+  console.log("dragenter");
+});
+
+dradAndDrop.addEventListener("dragleave", function (e) {
+  e.preventDefault();
+  dradAndDrop.classList.remove("active");
+  console.log("dragleave");
+});
+
+dradAndDrop.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+dradAndDrop.addEventListener("drop", function (e) {
+  e.preventDefault();
+  dradAndDrop.classList.remove("active");
+  // console.log("drop");
+  // console.log(e);
+  const files = e.dataTransfer.files;
+  // console.log(e.dataTransfer);
+  console.log(files);
+  for (let key in files) {
+    console.log(files[key]);
+    if (files[key].name && files[key].lastModified) {
+      filesList.push(files[key].name);
+      li = document.createElement("li");
+      li.className = "files-list";
+      li.innerHTML = `${files[key].name}`;
+      document.body.append(li);
+    }
+  }
+  console.log(filesList);
+
+  if (filesList.length > 0) {
+    uploadBtn.removeAttribute("disabled");
+  }
+});
+
+const uploadFiles = () => {
+  console.log("Files are downloaded");
+};
