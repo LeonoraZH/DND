3d76d9e55c3432ef36b36cacfa4fa9959c72d612
Leonora Zharova <norka_200@mail.rugit config --global user.namegit config --global user.email norka_200@mail.ru>
2023-01-18 10:59:34 +0300

    18.01.23

--- src/app.js
+++ src/app.js
@@ -24,17 +24,19 @@ dradAndDrop.addEventListener("dragover", function (e) {
   e.preventDefault();
 });
 
+let filesSize = 0;
 dradAndDrop.addEventListener("drop", function (e) {
   e.preventDefault();
   dradAndDrop.classList.remove("active");
-  // console.log("drop");
-  // console.log(e);
+  const remove = document.createElement("a");
   const files = e.dataTransfer.files;
-  // console.log(e.dataTransfer);
   console.log(files);
   for (let key in files) {
     console.log(files[key]);
+    console.log(files[key].size);
     if (files[key].name && files[key].lastModified) {
+      filesSize += files[key].size;
+      console.log(filesSize);
       filesList.push(files[key].name);
       li = document.createElement("li");
       li.className = "files-list";
@@ -47,6 +49,18 @@ dradAndDrop.addEventListener("drop", function (e) {
   if (filesList.length > 0) {
     uploadBtn.removeAttribute("disabled");
   }
+
+  let listElement = document.querySelectorAll(".files-list");
+  console.log(listElement);
+
+  function removeElement() {
+    console.log("it works");
+    console.log(this);
+    this.remove();
+  }
+  listElement.forEach(function (el) {
+    el.onclick = removeElement;
+  });
 });
 
 const uploadFiles = () => {
